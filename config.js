ab5b11ea802330571c3e6a04df458fcb20b06ed9
bithui12082003 <130596484+bithui12082003@users.noreply.github.com>
2024-11-19 00:40:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hi!',
+    introDesc: `Đố bạn biết nay ngày gièe`,
+    btnIntro: 'Lets find outtt',
+    title: 'Happy Birthday Happy Birthday Happy Birthday',
+    desc: 'Đố biếc luông  ',
+    btnYes: 'Sinh nhật Manh Manh',
+    btnNo: 'Quốc tế con traii',
+    question:'Tại sao bạn nhận được message này',
+    btnReply: 'Chịuuuuuuu',
+    reply: 'Tại dì Minh Anh xinh gái tốt bụng sẽ giúp toi có ni',
+    mess: 'Thoai vui thế thôi nhưng bố dặn con này, tuổi mới ngày càng xinh, học giỏi, chuyện tình cảm luôn được hạnh phúc bù đắp lại quá khứ nha',
+    messDesc: 'Hope u like this',
+    btnAccept: '1208200366639 - MB bank',
+    messLink: 'https://m.me/as.cacf' 
+}
